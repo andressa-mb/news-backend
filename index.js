6585af92791b51f77ca9767af9641c97c4043909
andressa-mb <andressamesquita26@gmail.com>
2024-02-05 21:11:47 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const usuarioModel = require('./src/module/usuario/usuario.model');
+const noticiaModel = require('./src/module/noticia/noticia.model');
+
+const cors = require('cors');
+const app = express();
+
+app.use(express.json());
+
+app.use(cors())
+
+app.post('/login', async (req, res) => {
+
+    //const {email, senha} = req.body;
+
+    //console.log(req.body.email, req.body.senha)
+
+    
+    if(!req.body.email) {
+        return res.status(400).json({message:"O campo email é obrigatório"});
+    }
+    if(!req.body.senha) {
+        return res.status(400).json({message: "O campo senha é obrigatório"});
+    }
+
+    const usuarioExistente = await usuarioModel.findOne({email: req.body.email})
+
+    if(!usuarioExistente){
+        return res.status(400).json({message: "Usuário não está cadastrado"});
+    }
+
+    const senhaVerificada = bcrypt.compareSync(
+        req.body.senha,
+        usuarioExistente.senha
+    );
+
+    if(!senhaVerificada) {
+        return res.status(400).json({message:"E-mail ou senha incorretos"});
+    }
+
+    const token = jwt.sign({ _id: usuarioExistente._id }, 'dnc');
+    console.log(token);
+
+    return res.status(200).json({
+        message: "Login realizado com sucesso",
+        token
+    });
+    
+
+});
+
+app.get('/usuarios', async (req, res) => {
+    const usuarios = await usuarioModel.find({});
+    return res.status(200).json(usuarios);
+});
+
+app.post('/usuarios', async (req, res) => {
+    if(!req.body.email){
+        return res.status(400).json({message: "O campo e-mail é obrigatório"});
+    }
+    if(!req.body.senha){
+        return res.status(400).json({message: "O campo senha é obrigatório"});
+    }
+
+    const usuarioExistente = await usuarioModel.find({email: req.body.email})
+
+    if(usuarioExistente.length){
+        return res.status(400).json({message: "Usuário já existe"});
+    }
+
+    const senhaCriptografada = bcrypt.hashSync(req.body.senha, 10);
+
+    const usuario = await usuarioModel.create({
+        nome: req.body.nome,
+        email: req.body.email,
+        senha: senhaCriptografada,
+    });
+
+    return res.status(201).json(usuario);
+});
+
+app.get('/noticias', async (req, res) => {
+    let filtroCategoria = {}
+    if(req.query.categoria){
+        filtroCategoria = {categoria: req.query.categoria};
+    }
+    const noticias = await noticiaModel.find(filtroCategoria);
+    return res.status(200).json(noticias);
+});
+
+app.post('/noticias', async (req, res) => {
+    if(!req.body.titulo){
+        return res.status(400).json({message: 'O campo título é obrigatório'})
+    }
+    if(!req.body.img){
+        return res.status(400).json({message: 'O campo imagem é obrigatório'})
+    }
+    if(!req.body.texto){
+        return res.status(400).json({message: 'O campo texto é obrigatório'})
+    }
+    if(!req.body.categoria){
+        return res.status(400).json({message: 'O campo categoria é obrigatório'})
+    }
+    
+    const noticia = await noticiaModel.create({
+        titulo: req.body.titulo,
+        img: req.body.img,
+        texto: req.body.texto,
+        categoria: req.body.categoria,
+    });
+
+    return res.status(201).json([noticia]);
+});
+
+app.listen(8080, () => {
+    console.log("Servidor funcionando na porta 8080");
+})
---
+++ src/module/noticia/noticia.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('../../config/mongo')
+const { Schema } = mongoose;
+
+const noticiaSchema = new Schema(
+  {
+  titulo: String,
+  img: String,
+  texto: String,
+  categoria: String,
+  },
+  { timestamps: true }
+);
+
+const noticiaModel = mongoose.model('noticias', noticiaSchema);
+
+module.exports =  noticiaModel;
